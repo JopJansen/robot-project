ca3131cc424e8cc648115c2a1edc86a09db01f20
WoutKlomp <wth.klomp@student.avans.nl>
2025-06-23 04:17:04 -0700

    ii

---
+++ rossem_ws/build/Manipulator/robot_arm_pkg/CMakeFiles/_robot_arm_pkg_generate_messages_check_deps_SorterenActionResult.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_robot_arm_pkg_generate_messages_check_deps_SorterenActionResult"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/_robot_arm_pkg_generate_messages_check_deps_SorterenActionResult.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rossem_ws/devel/share/common-lisp/ros/robot_arm_pkg/msg/_package_SorterenGoal.lisp
@@ -0,0 +1,6 @@
+(cl:in-package robot_arm_pkg-msg)
+(cl:export '(TF_FRAME-VAL
+          TF_FRAME
+          DOEL_POSITIE-VAL
+          DOEL_POSITIE
+))
