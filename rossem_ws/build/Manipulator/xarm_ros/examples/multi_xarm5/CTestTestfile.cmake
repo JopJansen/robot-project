a70ad7929fea3822058797b257773f685ac1c26d
WoutKlomp <wth.klomp@student.avans.nl>
2025-06-24 07:15:10 -0700

    ff

---
+++ rossem_ws/build/Manipulator/xarm_ros/examples/multi_xarm5/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/Git-projects/robot-project/rossem_ws/src/Manipulator/xarm_ros/examples/multi_xarm5
+# Build directory: /home/student/Git-projects/robot-project/rossem_ws/build/Manipulator/xarm_ros/examples/multi_xarm5
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ rossem_ws/build/Manipulator/xarm_ros/xarm_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/Git-projects/robot-project/rossem_ws/src/Manipulator/xarm_ros/xarm_gazebo
+# Build directory: /home/student/Git-projects/robot-project/rossem_ws/build/Manipulator/xarm_ros/xarm_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
