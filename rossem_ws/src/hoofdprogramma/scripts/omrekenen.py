79dae5cc18953c175de45707ab3bbd4beafbaef0
Student <student@avans.nl>
2025-06-24 02:56:29 -0700

    get in thereeee

---
+++ rossem_ws/src/hoofdprogramma/scripts/omrekenen.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+def vision_callback(msg):
+    """
+    Verwacht input als: kleur:x,y
+    Bijvoorbeeld: "rood:120,200"
+    """
+    data = msg.data.strip()
+    kleur, coords = data.split(":")
+    x, y = map(int, coords.split(","))
+
+    # Doelcoördinaten op basis van kleur
+    if kleur.lower() == "rood":
+        doel = "links_boven"
+    elif kleur.lower() == "groen":
+        doel = "links_onder"
+    elif kleur.lower() == "imbuus":
+        doel = "rechts_boven"
+    elif kleur.lower() == "geel":
+        doel = "rechts_onder"
+    else:
+        rospy.logwarn("Onbekende kleur: %s", kleur)
+        return
+
+    output = f"{kleur}:{x},{y} -> {doel}"
+    rospy.loginfo("Omgezet: %s", output)
+    output_pub.publish(output)
+
+rospy.init_node('coordinaten_omzetter')
+output_pub = rospy.Publisher('/coordinaten_output', String, queue_size=10)
+rospy.Subscriber('/vision_output', String, vision_callback)
+rospy.spin()
+
