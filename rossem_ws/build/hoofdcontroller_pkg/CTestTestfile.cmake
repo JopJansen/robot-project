5fb5368ec48f4937370a7a5d5d73287e2fe667d5
Student <student@avans.nl>
2025-06-27 06:15:28 -0700

    jaaa

---
+++ rossem_ws/build/hoofdcontroller_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/Git-projects/robot-project/rossem_ws/src/hoofdcontroller_pkg
+# Build directory: /home/student/Git-projects/robot-project/rossem_ws/build/hoofdcontroller_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ rossem_ws/build/hoofdprogramma/catkin_generated/installspace/hoofdprogramma.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python2
+import rospy
+from std_msgs.msg import String, Bool
+from your_package.msg import VisionResponse, RobotGoal  # vervang met juiste package en msg-namen
+from geometry_msgs.msg import Pose2D, PoseStamped
+import tf.transformations  # om quaternion naar euler te converteren
+
+
+class Hoofdcontroller:
+    def _init_(self):
+        rospy.init_node('hoofdcontroller')
+
+        # Publishers
+        self.vision_trigger_pub = rospy.Publisher('/vision/request_scan', Bool, queue_size=1)
+        self.robot_goal_pub = rospy.Publisher('/robot/goal', RobotGoal, queue_size=1)
+        self.transportband_pub = rospy.Publisher('/transportband/commando', String, queue_size=1)
+        self.coordinaten_pub = rospy.Publisher('/camera/coordinaten', Pose2D, queue_size=1)
+
+        # Subscribers
+        rospy.Subscriber('/hmi/input', String, self.hmi_callback)
+        rospy.Subscriber('/transportband/status', String, self.transportband_status_callback)
+        rospy.Subscriber('/vision/response', VisionResponse, self.vision_callback)
+        rospy.Subscriber('/camera/detected_pose', PoseStamped, self.camera_callback)  # PoseStamped nu
+
+        self.transportband_ready = False
+        self.awaiting_vision = False
+
+        rospy.loginfo("Hoofdcontroller gestart")
+        rospy.spin()
+
+    def hmi_callback(self, msg):
+        if msg.data == "START":
+            rospy.loginfo("START ontvangen van HMI")
+            self.transportband_pub.publish("START")
+
+    def transportband_status_callback(self, msg):
+        if msg.data == "READY":
+            rospy.loginfo("Transportband is klaar, trigger vision scan")
+            self.vision_trigger_pub.publish(True)
+            self.awaiting_vision = True
+
+    def vision_callback(self, msg):
+        # Je had deze functie waarschijnlijk al, maar zorg dat die er staat
+        rospy.loginfo("Vision response ontvangen")
+
+    def camera_callback(self, msg):
+        # Zet quaternion om naar yaw (theta)
+        q = msg.pose.orientation
+        euler = tf.transformations.euler_from_quaternion([q.x, q.y, q.z, q.w])
+        yaw = euler[2]  # yaw
+
+        # Zet PoseStamped om naar Pose2D met yaw
+        pose2d = Pose2D()
+        pose2d.x = msg.pose.position.x
+        pose2d.y = msg.pose.position.y
+        pose2d.theta = yaw
+
+        rospy.loginfo("Coördinaten ontvangen van camera: x=%.3f, y=%.3f, theta=%.3f" % (pose2d.x, pose2d.y, pose2d.theta))
+	self.coordinaten_pub.publish(pose2d)
+	rospy.loginfo("Coördinaten doorgestuurd naar /camera/coordinaten")
+
+
+
+if _name_ == '_main_':
+    try:
+        Hoofdcontroller()
+    except rospy.ROSInterruptException:
+        pass werkt dit programma ook 
